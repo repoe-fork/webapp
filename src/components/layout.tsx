624c7ea9b4a7f5f4e8e37735dd566374b1d0a63a
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-01-29 13:35:46 +0700

    color legend

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,19 +1,28 @@
-import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
-import React, { useMemo } from "react";
-import { Layout } from "../routes/areas";
+import {queryOptions, useSuspenseQuery} from "@tanstack/react-query";
+import React, {useMemo} from "react";
+import {Layout} from "../routes/areas";
+//@ts-ignore
+import palette from 'google-palette'
+import "color-legend-element";
+
+export interface IntrinsicElements {
+    "color-legend": any;
+}
 
 export const getLayout = (filename: string) =>
     queryOptions({
-        queryKey: ['layout', { filename }],
+        queryKey: ['layout', {filename}],
         queryFn: () => fetch(`https://repoe-fork.github.io/poe2/${filename}.json`).then(r => r.json()),
     })
 
-export const LayoutComponent: React.FC<{ layout: Layout }> = ({ layout }) => {
+export const LayoutComponent: React.FC<{ layout: Layout }> = ({layout}) => {
     const graph = useSuspenseQuery(getLayout(layout.file)).data
 
-    const [viewbox, scale] = useMemo(() => {
+    const [viewbox, scale, colorMap] = useMemo(() => {
+        const names = new Set<string>()
         const min = [Infinity, Infinity], max = [-Infinity, -Infinity]
-        for (let { x, y } of graph.nodes || []) {
+        for (let {x, y, room} of graph.nodes || []) {
+            names.add(room)
             min[0] = Math.min(min[0], x)
             min[1] = Math.min(min[1], y)
             max[0] = Math.max(max[0], x)
@@ -21,34 +30,53 @@ export const LayoutComponent: React.FC<{ layout: Layout }> = ({ layout }) => {
         }
         if (min[0] === max[0]) min[0] = 0
         if (min[1] === max[1]) min[1] = 0
-        return [`0 0 ${max[0] + min[0]} ${max[1] + min[1]}`, (max[0] - min[0]) / 200]
+        const colors: string[] = [...palette('mpn65', names.size)]
+        return [
+            `0 0 ${max[0] + min[0]} ${max[1] + min[1]}`,
+            (max[0] - min[0]) / 200,
+            Object.fromEntries([...names].sort().map(((n, i) => [n, '#' + colors[i]]))),
+        ]
     }, [graph.nodes])
+
 
     if (!graph.nodes?.length) {
         return <>No data</>
     }
 
-    return <div style={{ maxWidth: "500px" }}>
-        <svg viewBox={viewbox} style={{ width: "100%", border: "1px solid blue" }}>
-            {graph.edges.map(({ from, to, path, unknown }: any) => {
-                const start = graph.nodes[from]
-                const end = graph.nodes[to]
+    // @ts-ignore
+    // @ts-ignore
+    return <div>
+        <div style={{maxWidth: "500px"}}>
+            <svg viewBox={viewbox} style={{width: "100%", border: "1px solid blue"}}>
+                {graph.edges.map(({from, to, path, unknown}: any) => {
+                    const start = graph.nodes[from]
+                    const end = graph.nodes[to]
 
-                return <polyline
-                    points={`${start.x}, ${start.y} ${path.map(([x, y]: any) => `${x}, ${y}`).join(' ')} ${end.x}, ${end.y}`}
-                    strokeWidth={3 * scale}
-                    stroke="purple"
-                    strokeOpacity={0.6}
-                    fill="none"
-                >
-                    <title>{unknown?.find((v: any) => typeof v === 'string') || ""}</title>
-                </polyline>
-            })}
-            {graph.nodes.map(({ x, y, room }: any) =>
-                <circle cx={x} cy={y} r={(room ? 4 : 3) * scale} fill={room ? "green" : "lightblue"}>
-                    <title>{room || ""}</title>
-                </circle>
-            )}
-        </svg>
+                    return <polyline
+                        points={`${start.x}, ${start.y} ${path.map(([x, y]: any) => `${x}, ${y}`).join(' ')} ${end.x}, ${end.y}`}
+                        strokeWidth={3 * scale}
+                        stroke="gray"
+                        strokeOpacity={0.6}
+                        fill="none"
+                    >
+                        <title>{unknown?.find((v: any) => typeof v === 'string') || ""}</title>
+                    </polyline>
+                })}
+                {graph.nodes.map(({x, y, room}: any) =>
+                    <circle cx={x} cy={y} r={(room ? 4 : 3) * scale} fill={colorMap[room]}>
+                        <title>{room || ""}</title>
+                    </circle>
+                )}
+            </svg>
+        </div>
+        <div style={{float: "left", "--cle-columns": 1}}>
+            <color-legend
+                titleText={layout.file}
+                scaleType="categorical"
+                domain={Object.keys(colorMap)}
+                range={Object.values(colorMap)}
+            >
+            </color-legend>
+        </div>
     </div>
 }
